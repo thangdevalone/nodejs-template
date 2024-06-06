531dbbf0b77709081ef161ac9914a94cabc29f9d
ThangNguyen <101063068+thangdevalone@users.noreply.github.com>
2024-06-07 03:13:24 +0700

    update new version

--- src/app.js
+++ src/app.js
@@ -17,15 +17,8 @@ app.use(compression())
 
 //init db
 require('./db/init.mongodb.js')
-checkOverLoad()
 //init routes
-app.get('/',(req,res,next)=>{
-    const strCompress='Hello ThangDev'
-    return res.status(200).json({
-        message:'Welcome',
-        metadata:strCompress.repeat(10000)
-    })
-})
+app.use('',require('./routes'))
 //handling error
 
 
---
+++ src/controllers/access.controller.js
@@ -0,0 +1,19 @@
+'use strict';
+
+class AccessController{
+
+    signUp = async (req,res ,next)=>{
+        try{
+            console.log('[P]::signUp::',req.body);
+            return res.status(200).json({
+                code:'2001',
+                metadata:{userId:1}
+            })
+        }
+        catch (error){
+            next(error)
+        }
+    }
+}
+
+module.exports=new AccessController();
